8b30eef3b1dab36fdb83ed287d7ce9cf2f931e58
emersonzx <emersonmv27@gmail.com>
2022-12-19 03:39:05 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter as BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import Projects from './components/pages/Projects'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Project from './components/pages/Project'
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+    <Navbar/>
+     <Container customClass="min-height">
+     <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/newproject" element={<NewProject/>}></Route>
+          <Route path="/project/:id" element={<Project/>}></Route>
+        </Routes>
+     </Container>
+     <Footer/>
+    </BrowserRouter>
+  );
+} 
+
+export default App;
